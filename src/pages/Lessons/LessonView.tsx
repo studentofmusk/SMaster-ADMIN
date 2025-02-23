4f43dd0ece2298ff3d68a696b1c38fae31d76958
Farooq Khan <farooqvrofficial@gmail.com>
2025-02-23 21:33:43 +0530

    remove unused imports

--- src/pages/Lessons/LessonView.tsx
+++ src/pages/Lessons/LessonView.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { Card, LectureCard, LessonCard, ListDisplay, T2ActionCard, T2VideoCard, V2ActionCard, V2TextCard } from "../../components/Tools";
+import { LectureCard, T2ActionCard, T2VideoCard, V2ActionCard, V2TextCard } from "../../components/Tools";
 import { IGroup } from "../Groups";
 import { useAPI } from "../../hooks/useAPI";
 import { get_groups, get_languages, get_lectures, get_lessons, get_seasons, get_t2action, get_t2video, get_v2action, get_v2text, get_videos} from "../../utils/apis";
@@ -120,141 +120,149 @@ export default function LessonView({setPath}:{setPath:(path: string)=>any}) {
   return (
     <section className="px-10 h-[80vh]">
       <h2 className="text-[#EB5A3C] uppercase font-bold">View Lesson</h2>
-
-      <table className="mt-4">
-        <tbody>
-            <tr>
-                <td className="px-10 py-1 uppercase text-[#DF9755]">Language:</td>
-                <td className="px-10 py-1 uppercase text-gray-500 ">{language?.title || "Not Found"}</td>
-            </tr>
-            
-            <tr>
-                <td className="px-10 py-1 uppercase text-[#DF9755]">Season:</td>
-                <td className="px-10 py-1 uppercase text-gray-500 ">{season?.title || "Not Found"}</td>
-            </tr>
-
-            <tr>
-                <td className="px-10 py-1 uppercase text-[#DF9755]">Group:</td>
-                <td className="px-10 py-1 uppercase text-gray-500 ">{group?.title || "Not Found"}</td>
-            </tr>
-
-            <tr>
-                <td className="px-10 py-1 uppercase text-[#DF9755]">Lesson Type:</td>
-                <td className="px-10 py-1 uppercase text-gray-500 ">{lesson?.lesson_type}</td>
-            </tr>
-
-            <tr>
-                <td className="px-10 py-1 uppercase text-[#DF9755]">Total XP:</td>
-                <td className="px-10 py-1 uppercase text-gray-500 ">{lesson?.total_xp || 0}</td>
-            </tr>
-
-
-        </tbody>
-      </table>
-      
-      <div className="mt-4 h-[70%] items-start overflow-y-scroll flex flex-wrap space-x-2 space-y-2">
-        
         {
-            lesson && lesson.topics.length > 0
-            ?
-            lesson.topics.map((topic)=>{
-                
-                
-                switch (topic.topic_type){
-                    case (TopicTypes.LECTURE):{
+         loading?<div>Loading...</div>
+         :(
+            error?<div>Error:</div>
+            :<>
+            
+            <table className="mt-4">
+                <tbody>
+                    <tr>
+                        <td className="px-10 py-1 uppercase text-[#DF9755]">Language:</td>
+                        <td className="px-10 py-1 uppercase text-gray-500 ">{language?.title || "Not Found"}</td>
+                    </tr>
+                    
+                    <tr>
+                        <td className="px-10 py-1 uppercase text-[#DF9755]">Season:</td>
+                        <td className="px-10 py-1 uppercase text-gray-500 ">{season?.title || "Not Found"}</td>
+                    </tr>
 
-                        let lecture = lectureMap?.get(topic.topic_id);
-                        let VIDEO =  videoMap?.get(lecture?.video || "");
-                        return (
-                            <>{VIDEO && lecture
-                                ?<LectureCard  
-                                title={lecture.title}
-                                url={VIDEO.url}
-                                thumbnail={VIDEO.thumbnail}
-                                audio={VIDEO.audio}
-                                />
-                                :<></>
+                    <tr>
+                        <td className="px-10 py-1 uppercase text-[#DF9755]">Group:</td>
+                        <td className="px-10 py-1 uppercase text-gray-500 ">{group?.title || "Not Found"}</td>
+                    </tr>
+
+                    <tr>
+                        <td className="px-10 py-1 uppercase text-[#DF9755]">Lesson Type:</td>
+                        <td className="px-10 py-1 uppercase text-gray-500 ">{lesson?.lesson_type}</td>
+                    </tr>
+
+                    <tr>
+                        <td className="px-10 py-1 uppercase text-[#DF9755]">Total XP:</td>
+                        <td className="px-10 py-1 uppercase text-gray-500 ">{lesson?.total_xp || 0}</td>
+                    </tr>
+
+
+                </tbody>
+            </table>
+            
+            <div className="mt-4 h-[70%] items-start overflow-y-scroll flex flex-wrap space-x-2 space-y-2">
+                
+                {
+                    lesson && lesson.topics.length > 0
+                    ?
+                    lesson.topics.map((topic)=>{
+                        
+                        
+                        switch (topic.topic_type){
+                            case (TopicTypes.LECTURE):{
+
+                                let lecture = lectureMap?.get(topic.topic_id);
+                                let VIDEO =  videoMap?.get(lecture?.video || "");
+                                return (
+                                    <>{VIDEO && lecture
+                                        ?<LectureCard  
+                                        title={lecture.title}
+                                        url={VIDEO.url}
+                                        thumbnail={VIDEO.thumbnail}
+                                        audio={VIDEO.audio}
+                                        />
+                                        :<></>
+                                    }
+                                    
+                                    </>
+                                )
                             }
                             
-                            </>
-                        )
-                    }
-                    
-                    case (TopicTypes.V2TEXT):{
+                            case (TopicTypes.V2TEXT):{
 
-                        let v2text = v2textMap?.get(topic.topic_id);
-                        let VIDEO = videoMap?.get(v2text?.video || "");
-                        return (
-                            <>{VIDEO && v2text
-                                ?<V2TextCard  
-                                title={v2text.title}
-                                url={VIDEO.url}
-                                thumbnail={VIDEO.thumbnail}
-                                audio={VIDEO.audio}
-                                options={v2text.options}
-                                />
-                                :<></>
-                            }
-                            </>
-                        )
-                    }
-                    
-                    case (TopicTypes.T2VIDEO):{
-                        let t2video = t2videoMap?.get(topic.topic_id);
-                        const VIDEO_1 = videoMap.get(t2video?.options[0]|| "");
-                        const VIDEO_2 = videoMap.get(t2video?.options[1]|| "");
-                        const VIDEO_3 = videoMap.get(t2video?.options[2]|| "");
-                        const VIDEO_4 = videoMap.get(t2video?.options[3]|| "");
-                        return (
-                                <T2VideoCard  
-                                title={t2video?.title || "No Title"}
-                                options={[VIDEO_1, VIDEO_2, VIDEO_3, VIDEO_4]}
-                                />
-                            );
-                        
-                    }
-                    case (TopicTypes.V2ACTION):{
-                        
-                        let v2action = v2actionMap?.get(topic.topic_id);
-                        let VIDEO = videoMap.get(v2action?.video || "");
-                        return (
-                            <>{VIDEO && v2action
-                                ?<V2ActionCard  
-                                    title={v2action.title}
-                                    url={VIDEO.url}
-                                    thumbnail={VIDEO.thumbnail}
-                                    action_id={VIDEO.action_id}
-                                    />
-                                :<></>
-                            }
-                            </>
-                        )
-                    }
-                    
-                    case (TopicTypes.T2ACTION):{
-                        let t2action = t2actionMap?.get(topic.topic_id);
-                        const VIDEO = videoMap.get(t2action?.video || "");
-                        return (
-                            <>{VIDEO && t2action
-                                ?<T2ActionCard  
-                                    title={t2action.title}
-                                    action_id={VIDEO.action_id}
-                                    />
-                                :<></>
+                                let v2text = v2textMap?.get(topic.topic_id);
+                                let VIDEO = videoMap?.get(v2text?.video || "");
+                                return (
+                                    <>{VIDEO && v2text
+                                        ?<V2TextCard  
+                                        title={v2text.title}
+                                        url={VIDEO.url}
+                                        thumbnail={VIDEO.thumbnail}
+                                        audio={VIDEO.audio}
+                                        options={v2text.options}
+                                        />
+                                        :<></>
+                                    }
+                                    </>
+                                )
                             }
                             
-                            </>
-                        )
-                    }
-                    default:
-                        return  "";
-                    }
-            })
-            :"No Topics Found"
+                            case (TopicTypes.T2VIDEO):{
+                                let t2video = t2videoMap?.get(topic.topic_id);
+                                const VIDEO_1 = videoMap.get(t2video?.options[0]|| "");
+                                const VIDEO_2 = videoMap.get(t2video?.options[1]|| "");
+                                const VIDEO_3 = videoMap.get(t2video?.options[2]|| "");
+                                const VIDEO_4 = videoMap.get(t2video?.options[3]|| "");
+                                return (
+                                        <T2VideoCard  
+                                        title={t2video?.title || "No Title"}
+                                        options={[VIDEO_1, VIDEO_2, VIDEO_3, VIDEO_4]}
+                                        />
+                                    );
+                                
+                            }
+                            case (TopicTypes.V2ACTION):{
+                                
+                                let v2action = v2actionMap?.get(topic.topic_id);
+                                let VIDEO = videoMap.get(v2action?.video || "");
+                                return (
+                                    <>{VIDEO && v2action
+                                        ?<V2ActionCard  
+                                            title={v2action.title}
+                                            url={VIDEO.url}
+                                            thumbnail={VIDEO.thumbnail}
+                                            action_id={VIDEO.action_id}
+                                            />
+                                        :<></>
+                                    }
+                                    </>
+                                )
+                            }
+                            
+                            case (TopicTypes.T2ACTION):{
+                                let t2action = t2actionMap?.get(topic.topic_id);
+                                const VIDEO = videoMap.get(t2action?.video || "");
+                                return (
+                                    <>{VIDEO && t2action
+                                        ?<T2ActionCard  
+                                            title={t2action.title}
+                                            action_id={VIDEO.action_id}
+                                            />
+                                        :<></>
+                                    }
+                                    
+                                    </>
+                                )
+                            }
+                            default:
+                                return  "";
+                            }
+                    })
+                    :"No Topics Found"
+                }
+
+            
+                </div>
+            </>
+         )   
         }
-
-      
-    </div>
     </section>
   )
 }
